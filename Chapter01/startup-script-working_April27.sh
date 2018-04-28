2ab4a4acd9b08f73b531b41f3f70f942606e4156
root <root@nodek66.maprtech.local>
2018-04-28 01:33:05 -0400

    mongo db to listen on 0.0.0.0

--- Chapter01/startup-script-working_April27.sh
+++ Chapter01/startup-script-working_April27.sh
@@ -41,11 +41,63 @@ ExecStart=/usr/bin/mongod --quiet --config /etc/mongod.conf
 [Install]
 WantedBy=multi-user.target
 EOF
-cp /etc/mongod.conf /etc/mongod.conf_Orig
-sed -i 's/bindIp: 127.0.0.1/bindIp: 0.0.0.0/g' /etc/mongod.conf
 systemctl start mongodb
 systemctl enable mongodb
 
 
+
+# Install dependencies from apt
+apt-get install -yq ca-certificates git  build-essential supervisor
+
+
+curl -sL https://deb.nodesource.com/setup_6.x -o nodesource_setup.sh
+bash nodesource_setup.sh
+apt-get install -y nodejs
+apt-get install -y gcc g++ make
+
+# Install nodejs
+mkdir /opt/nodejs
+curl https://nodejs.org/dist/v4.2.2/node-v4.2.2-linux-x64.tar.gz | tar xvzf - -C /opt/nodejs --strip-components=1
+ln -s /opt/nodejs/bin/node /usr/bin/node
+ln -s /opt/nodejs/bin/npm /usr/bin/npm
+
+# Get the application source code from the Google Cloud Repository.
+# git requires $HOME and it's not set during the startup script.
+export HOME=/root
+
+
+git config --global credential.helper gcloud.sh
+#git clone https://source.developers.google.com/p/gcp-cookbook/r/gcpcookbook  /opt/app
+git clone https://source.developers.google.com/p/krishna-gcp-april24/r/gcp-cookbook  /opt/app
+
+# Install app dependencies
+cd /opt/app/Chapter01/mysite
+npm install
+cat >./.env << EOF
+COOKIE_SECRET=d44d5c45e7f8149aabc06a830dba5716b4bd952a639c82499954
+MONGODB_URI=mongodb://localhost:27017
+EOF
+
+# Create a nodeapp user. The application will run as this user.
+useradd -m -d /home/nodeapp nodeapp
+chown -R nodeapp:nodeapp /opt/app
+
+# Configure supervisor to run the node app.
+cat >/etc/supervisor/conf.d/node-app.conf << EOF
+[program:nodeapp]
+directory=/opt/app/Chapter01/mysite
+command=npm start
+autostart=true
+autorestart=true
+user=nodeapp
+environment=HOME="/home/nodeapp",USER="nodeapp",NODE_ENV="production"
+stdout_logfile=syslog
+stderr_logfile=syslog
+EOF
+
+supervisorctl reread
+supervisorctl update
+
+# Application should now be running under supervisor
 # [END startup]
 
